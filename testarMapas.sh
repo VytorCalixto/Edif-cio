b776d1186fd4b482589f8ce109c4e0b173830a59
Vytor Calixto <vsbc14@inf.ufpr.br>
2014-10-13 16:04:50 -0300

    Comentários, .sh para testes

---
+++ testarMapas.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+wget http://www.inf.ufpr.br/luciano/grad/ci067_2014_2/site.tar.gz
+tar -xzf site.tar.gz
+rm site.tar.gz
+make
+clear
+
+FILES=site/*.out
+for f in $FILES
+do
+	name=${f%\.*}
+	output=$(time ./fogo < $(echo "$name.in"))
+	result=$(echo "$output" | tail -1)
+	expected=$(cat $(echo "$name.out"))
+	if [ "$result" != "$expected" ]; then
+		echo -e "\e[0;31mErro no teste $name.\e[0m"
+	else
+		echo -e "\e[0;32mTeste $name ok!\e[0m"
+	fi
+	echo -e "Esperado: $expected. Retornado: $result."
+done
+
+rm -fr site
