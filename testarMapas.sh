4de50fa9b1530d690ef24c8b3d279f3f38b4e215
Vytor Calixto <vsbc14@inf.ufpr.br>
2014-10-20 18:21:44 -0200

    Modificado testarMapas.sh para usar mais casos de testes (os que dão erros)

--- testarMapas.sh
+++ testarMapas.sh
@@ -3,10 +3,28 @@
 
 
 wget http://www.inf.ufpr.br/luciano/grad/ci067_2014_2/site.tar.gz
+wget http://www.inf.ufpr.br/luciano/grad/ci067_2014_2/avaliacao.tar.gz
 tar -xzf site.tar.gz
+tar -xzf avaliacao.tar.gz
+rm avaliacao.tar.gz
 rm site.tar.gz
 make
 clear
+
+FILES=avaliacao/*.out
+for f in $FILES
+do
+	name=${f%\.*}
+	output=$(time ./fogo < $(echo "$name.in"))
+	result=$(echo "$output" | tail -1)
+	expected=$(cat $(echo "$name.out"))
+	if [ "$result" != "$expected" ]; then
+		echo -e "\e[0;31mErro no teste $name.\e[0m"
+	else
+		echo -e "\e[0;32mTeste $name ok!\e[0m"
+	fi
+	echo -e "Esperado: $expected. Retornado: $result."
+done
 
 FILES=site/*.out
 for f in $FILES
@@ -24,4 +42,5 @@ do
 done
 
 rm -fr site
+rm -fr avaliacao
 
